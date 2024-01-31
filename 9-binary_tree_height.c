c543b087311fba41ac5ad3b48f1e5c4838af4300
sooma93 <tasneemabasit5@gmail.com>
2024-01-31 11:51:16 -0800

    task 10

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t i = 0 , j = 0;
+	size_t i = 0, j = 0;
 
-		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((i > j) ? i : j);
+	i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((i > j) ? i : j);
 	}
 	return (0);
 }
